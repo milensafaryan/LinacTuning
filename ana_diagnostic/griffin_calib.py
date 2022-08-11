a3f549ac049f637937302a47ef6b09432647b271
rsharankova <ralitsa.sharankova@gmail.com>
2022-08-11 10:32:08 -0600

    update griffin plotter

--- ana_diagnostic/griffin_calib.py
+++ ana_diagnostic/griffin_calib.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 
+plt.rcParams["font.family"] = "Times New Roman"
+plt.rcParams["mathtext.fontset"] = 'cm'
+
 t = [None]*4
 x = [None]*4 
 xplat = [None]*4
@@ -13,8 +16,8 @@ dt  = [219.782, 220.010, 220.243, 220.487] # external input: from finddt.py
 path = r'/Users/rshara01-local/Desktop/Winter 2022/Study 12302021/2study12302021/'
 suffix = '_ALL.csv'
 
-for i in range(4):
-    
+
+for i in range(4):    
     filename = path + r'%dfront3'%i + suffix
     file = io.open('%s'%filename,encoding="utf-8")
     lines = file.readlines()[12:]
@@ -39,12 +42,10 @@ for i in range(4):
 
     print(med[i])
 
-
-nmz = ['1back1','1back2','1back3','2back1','3back1','8nsboth2']
+nmz = ['8nsboth2','1back3','2back1','3back1'] #1back1 1back2
+labels = ['No delay','2.125\"','4.25\"','6.375\"']
 filenm = ['./files/%s.npz'%nmz[i] for i in range(len(nmz))]
 data = [np.load(f) for f in filenm]
-
-print(data[0]['pxx'][0])
 
 dT =[None]*len(nmz)
 
@@ -56,18 +57,23 @@ for i,d in enumerate(data):
     dT[i] = np.absolute(dT[i])
 
 
-plt.xlabel('peaks')
-plt.ylabel('time diff (8e-11)')
+plt.xlabel('# Trace peaks',fontsize='xx-large')
+plt.ylabel('$\Delta$T (ns)',fontsize='xx-large')
+plt.xticks(fontsize='x-large')
+plt.yticks(fontsize='x-large')
 plt.grid(color='k', linestyle='-', linewidth=2)
-[ plt.plot(dT[i]/0.8e-10, label='%s'%nmz[i]) for i in range(len(dT))]
-plt.legend()
+[ plt.plot(dT[i]/1.e-9, label='%s'%labels[i]) for i in range(len(dT))]
+plt.legend(fontsize='x-large')
 plt.show()
 
-plt.title("Phase det. signal")
-plt.xlabel('T')
-plt.ylabel('V')
+plt.title("Phase det. signal",fontsize='xx-large')
+plt.xlabel('Time ($\mu$)s',fontsize='xx-large')
+plt.ylabel('Signal (V)',fontsize='xx-large')
+plt.xticks(fontsize='x-large')
+plt.yticks(fontsize='x-large')
 plt.grid(color='k', linestyle='-', linewidth=2)
-[plt.plot(t[i], x[i], marker = '',label='') for i in range(4)]
+[plt.plot(t[i]*1e6, x[i], marker = '',label='%s'%labels[i]) for i in range(4)]
+plt.legend(fontsize='x-large')
 plt.show()
 
 plt.grid(color='k', linestyle='-', linewidth=2)
@@ -75,7 +81,7 @@ plt.grid(color='k', linestyle='-', linewidth=2)
 plt.show()
 
 terr = [0.08]*4
-coef = np.polyfit(dt,med,1,w=terr)
+coef = np.polyfit(dt,med,1,w=1/terr)
 poly1d_fn = np.poly1d(coef) 
 
 plt.title("Griffin calibration")
@@ -87,4 +93,3 @@ plt.errorbar(dt,med, xerr=terr, linestyle='', c='b')
 #plt.plot(dt,med)
 plt.show()
 print(coef)
-
